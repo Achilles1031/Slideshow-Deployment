9a0668b2bc7dad85a77ff0623bd453dfb86e8eef
Achilles1031 <adelgado@student.neumont.edu>
2024-01-30 14:30:01 -0700

    Initial Website Commit
    Added Home Page Slide Show

---
+++ js/site.js
@@ -0,0 +1,18 @@
+console.log ("js loaded")
+
+//set interval will call a function every Nms
+
+// let count = 0;
+
+// function printCountAndIncrement(){
+//     count++
+//     console.log(count)
+// }
+
+// setInterval(printCountAndIncrement, 3000)
+
+let imageURLs = [
+    "https://www.akc.org/wp-content/uploads/2017/11/Bulldog-standing-in-the-grass.jpg"
+    "https://www.thesprucepets.com/thmb/zXkzVVV5P8h2JG0ZUFtXtvIq-lM=/3600x0/filters:no_upscale():strip_icc()/bulldog-4584344-hero-8b60f1e867f046e792ba092eec669256.jpg"
+    "https://media.istockphoto.com/id/855841098/photo/english-bulldog.jpg?s=612x612&w=0&k=20&c=MH8THQVlDgsnFVgmlAD2PtsYC08FSiKKmuSIa2UkmFs="
+];
